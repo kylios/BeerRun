1c1c5f065a7415387a801082233551bf5c1cc6b2
Kyle Racette <kracette@gmail.com>
2013-03-01 08:06:03 -0800

    Amplified the theme song music.  Added some comments in UI

--- lib/ui.dart
+++ lib/ui.dart
@@ -5,6 +5,38 @@ import 'dart:html';
 import 'package:BeerRun/input.dart';
 import 'package:BeerRun/drawing.dart';
 
+/**
+ * Ok, let's not do UI in canvas.  Let's take advantage of the fact we're on a
+ * web browser and do the UI with html and css (see
+ * http://stackoverflow.com/questions/6856953/does-it-make-sense-to-create-canvas-based-ui-components
+ * for inspiration).
+ *
+ * What we need:
+ * - view - something that can be drawn
+ * - layout - can arrange elements and position them
+ * - listener - captures events from a particular element
+ *
+ *
+ * class Layout {
+ *
+ *
+ * }
+ *
+ * class View {
+ *
+ *   View(View parent, /* data */ ) {
+ *     // set up data
+ *
+ *   }
+ *
+ *   void draw() {
+ *
+ *     // create html and append to parent
+ *
+ *   }
+ * }
+ */
+
 part 'src/ui/view.dart';
 part 'src/ui/dialog.dart';
 part 'src/ui/ui.dart';
