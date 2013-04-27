df7d7230844e4043c2f52dee35b65b719b187015
Kyle Racette <kracette@gmail.com>
2013-04-27 13:13:33 -0700

    tool to push code to new server

---
+++ tools/push2.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+SERVER_HOST=test.kyleracette.com
+UPLOAD_DIR=/home/kyle/.web_uploads
+UPLOAD_USER=kyle
+WEB_DIR=../web
+OUTFILE=beer_run.dart.js
+INFILE=beer_run.dart
+
+if [ "$1" = dev ] || [ "$1" = DEV ]; then
+	CMD="dart2js --checked --analyze-all --disallow-unsafe-eval --enable-diagnostic-colors --out=$OUTFILE $INFILE"
+else
+	CMD="dart2js --analyze-only --minify --out=$OUTFILE $INFILE"
+fi
+
+#cd $WEB_DIR
+#$CMD
+
+rsync -rv ../web/* $UPLOAD_USER@$SERVER_HOST:$UPLOAD_DIR
+ssh $UPLOAD_USER@$SERVER_HOST "mv $UPLOAD_DIR/* /opt/www/$SERVER_HOST/public_html/"
+
