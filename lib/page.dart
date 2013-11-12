5fefa34405da98b76394bb2d956fb36c5dcec92e
Kyle Racette <kracette@gmail.com>
2013-11-12 00:05:51 -0800

    added some detail to the buildings.  Added files to start a page class, for managing page elements.  oh yeah, fps counter broke somewhere in there.

---
+++ lib/page.dart
@@ -0,0 +1,6 @@
+library page;
+
+import 'dart:html';
+
+part 'src/page/page.dart';
+part 'src/page/page_config.dart';
---
+++ lib/src/page/page.dart
@@ -0,0 +1,9 @@
+part of page;
+
+class Page {
+
+  Map<String, Element> _elements = new Map<String, Element>();
+  PageConfig _config;
+
+  Page(this._config);
+}
