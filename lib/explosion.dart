0460cd0c17da1262aae4a977104d859b8558e776
Kyle Racette <kracette@gmail.com>
2013-11-29 14:11:27 -0500

    ...and the explosion too

--- lib/explosion.dart
+++ lib/explosion.dart
@@ -1,8 +1,5 @@
 library explosion;
 
-import 'dart:html';
-
-import 'package:beer_run/level.dart';
 import 'package:beer_run/drawing.dart';
 import 'package:beer_run/animation.dart';
 
@@ -11,22 +8,22 @@ class Explosion extends GraphicAnimation {
   factory Explosion.createAt(int x, int y, int width, int height) {
     SpriteSheet sheet = new SpriteSheet('img/explosion.png', 128, 128);
     List<Sprite> sprites = [
-      sheet.spriteAt(0, 0),
-      sheet.spriteAt(128, 0),
-      sheet.spriteAt(256, 0),
-      sheet.spriteAt(384, 0),
-      sheet.spriteAt(0, 128),
-      sheet.spriteAt(128, 128),
-      sheet.spriteAt(256, 128),
-      sheet.spriteAt(384, 128),
-      sheet.spriteAt(0, 256),
-      sheet.spriteAt(128, 256),
-      sheet.spriteAt(256, 256),
-      sheet.spriteAt(384, 256),
-      sheet.spriteAt(0, 384),
-      sheet.spriteAt(128, 384),
-      sheet.spriteAt(256, 384),
-      sheet.spriteAt(384, 384)
+      sheet.spriteAtNew(0, 0),
+      sheet.spriteAtNew(0, 1),
+      sheet.spriteAtNew(0, 2),
+      sheet.spriteAtNew(0, 3),
+      sheet.spriteAtNew(1, 0),
+      sheet.spriteAtNew(1, 1),
+      sheet.spriteAtNew(1, 2),
+      sheet.spriteAtNew(1, 3),
+      sheet.spriteAtNew(2, 0),
+      sheet.spriteAtNew(2, 1),
+      sheet.spriteAtNew(2, 2),
+      sheet.spriteAtNew(2, 3),
+      sheet.spriteAtNew(3, 0),
+      sheet.spriteAtNew(3, 1),
+      sheet.spriteAtNew(3, 2),
+      sheet.spriteAtNew(3, 3)
                             ];
 
     SpriteAnimation anim = new SpriteAnimation(sprites, false);
